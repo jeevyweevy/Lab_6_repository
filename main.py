c117e57f38a928781e14e0123db3d6ebc6bce7b9
Pierce Maloney <piercemaloney@ufl.edu>
2023-03-19 18:46:10 -0400

    Added a decode function

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ def encode(password):
         encoded_string += encoded_value
     return encoded_string
 
+def decode(password):
+    str_password = str(password)
+    dencoded_string = ""
+    for num in str_password:
+        dencoded_value = str((int(num) - 3) % 10)
+        dencoded_string += dencoded_value
+    return dencoded_string
+
 if __name__ == '__main__':
     while True:
         print(f'Menu'
